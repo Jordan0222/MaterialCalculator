73db77116094fe01b9bc0b0b908aa43224b1a19a
zhangyixun <asxz0121@gmail.com>
2022-10-16 19:33:56 +0800

    Add a textView

--- app/src/main/java/com/example/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/example/materialcalculator/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.materialcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.materialcalculator.ui.theme.MaterialCalculatorTheme
@@ -17,7 +20,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialCalculatorTheme {
-                
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "hello Jordan"
+                    )
+                }
             }
         }
     }
